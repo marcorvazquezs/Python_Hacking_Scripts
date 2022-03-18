4a5009e545a652b4bef817d2be9beaac19f3efd0
Marco Vazquez <marcorvazquezs@gmail.com>
2022-03-18 16:14:12 -0700

    uptime sensor

--- 401_Scripts/uptime_sensor.py
+++ 401_Scripts/uptime_sensor.py
@@ -1,27 +1,71 @@
-#!/usr/bin/python3
+#!/bin/python3
 
-import os, time, datetime;
+# Script:                   401 Ops Challenge Uptime Sensor 
+# Author:                   Marco Vazquez
+# Date of latest revision:  3/18/2022
+# Purpose:                  Uptime Sensor Pt 2 of 2
+
+from http import server
+import smtplib;
+import os, time, datetime
+from sqlite3 import Timestamp;
+from getpass import getpass;
+
+# Declare variables 
+up = "Network is ACTIVE"
+down = "Network is DOWN"
+last = 0
+ping_result = 0
+email = input("Enter your email: ")
+password = getpass()
+ip = input("What IP Address would you like to monitor?")
 
 # Declare functions
 
-def check_ping(target):
-    response = os.system("ping -c 1 " + target)
+# Send the emails
 
-    # check the response
-    print(response)
+def send_upAlert():
+    now = datetime.datetime.now()
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(email, password)
+    up_messsage = "Your system has recovered. \n %s" % now 
+    server.sendmail('statusbot@myservers.com', email, up_messsage)
+    server.quit()
+
+def send_downAlert():
+    now = datetime.datetime.now()
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(email, password)
+    down_message = "Your system is DOWN. \n %s" + now
+    server.sendmail('statusbot@myservers.com', email, down_message)
+    server.quit()
+    
+
+def ping():
+    now = datetime.datetime.now()
+
+    global ping_result
+    global last
+
+    if ((ping_result != last) and (ping_result == up)):
+        last = up
+        send_upAlert()
+    elif ((ping_result != last) and (ping_result == down)):
+        send_downAlert()
+        last = down
+    
+    response = os.system("ping -c 1 " + ip)
     if response == 0:
-        pingstatus = "Network Active"
+        ping_result = up
     else: 
-        pingstatus = "Network Error"
-
-    return pingstatus
-
-# Handle function output
-
-pingstatus = check_ping("8.8.8.8")
-
+        ping_result = down
+    print(now + ping_result + " to " + ip)
+ 
 # Infinite ping loop 
 while True:
-    now = datetime.datetime.now()
-    print(str(now) + " " + pingstatus + " to 8.8.8.8")
+    ping()
     time.sleep(2)
+
+
